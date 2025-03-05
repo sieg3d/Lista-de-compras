e494611ea88f8e6c87d0de4f5c755f3bfcdd9ef8
Douglas Lopes <douglas.lopes@soulasalle.com.br>
2025-03-05 03:26:43 -0300

    update

---
+++ navbar.js
@@ -0,0 +1,23 @@
+document.write(`
+    <div class="topnav">
+      <a href="#home" class="active">
+        <img src="logo.png" id="oi"> Olá, Fulane!
+      </a>
+  
+      <a href="#" class="notification">
+        <i class="fa fa-bell"></i>
+        <span class="badge">3</span>
+      </a>
+  
+      <a href="javascript:void(0);" class="icon" onclick="myFunction()">
+        <i class="fa fa-bars"></i>
+      </a>
+  
+      <div id="myLinks">
+        <a href="cadastrar_produto.html">Cadastrar Produtos</a>
+        <a href="#">Estoque</a>
+        <a href="#">Sair</a>
+      </div>
+    </div>
+  `);
+  
