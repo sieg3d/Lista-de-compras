005e1356c9d9525cf8654fe9cc63c293352489a5
Douglas Lopes <douglas.lopes@soulasalle.com.br>
2025-05-13 00:33:11 -0300

    update sw

---
+++ service-worker.js
@@ -0,0 +1,36 @@
+const CACHE_NAME = 'lista-app-cache-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/listas.html',
+  '/estoque.html',
+  '/style.css'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys.map(key => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      )
+    )
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
